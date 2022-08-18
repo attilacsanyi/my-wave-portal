21028ed16599f0a9f25a20298cf76f53009da89e
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-08-18 14:33:03 +0200

    build: add run script and empty contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("This is my awesome first contract!");
+    }
+}
