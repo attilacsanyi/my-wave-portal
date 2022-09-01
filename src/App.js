1c06bef01404d1c56068ad378c22c34aa3a27455
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-09-01 21:22:06 +0200

    build: call total waves and wave contract methods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 const App = () => {
   /** Just a state variable we use to store our user's public wallet.*/
   const [currentAccount, setCurrentAccount] = useState("");
+
+  /**
+   * Create a variable here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0x145a579c29f72b37b1e2d85eb893307b6e0b0256";
+  /**
+   * Create a variable here that references the abi content!
+   */
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -59,7 +70,39 @@ const App = () => {
     checkIfWalletIsConnected();
   }, []);
 
-  const wave = () => {};
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        /*
+         * Execute the actual wave from your smart contract
+         */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="mainContainer">
